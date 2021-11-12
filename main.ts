40bdf4975408c44c2808be98f7d01a648a902a34
Desoppere <59663882+Desoppere@users.noreply.github.com>
2021-11-12 12:22:31 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -7,30 +7,121 @@ input.onGesture(Gesture.LogoDown, function () {
         # # # # #
         `)
 })
+// eten geven
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # # . # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # # # # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # # . # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # # # # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
-        . # # # .
-        . . # . .
+        . . . . .
+        . . . . .
         . . . . .
         . # # # .
         # . . . #
         `)
     basic.pause(1000)
     basic.showLeds(`
-        . # # # .
-        . . # . .
+        . . . . .
+        . . . . .
         . . . . .
         # # # # #
         . . . . .
         `)
 })
+// aaien
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
 basic.showLeds(`
-    . # # # .
-    . . # . .
+    . . . . .
+    . . . . .
     . . . . .
     # . . . #
     . # # # .
     `)
 basic.forever(function () {
-	
+    basic.pause(5000)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
 })
